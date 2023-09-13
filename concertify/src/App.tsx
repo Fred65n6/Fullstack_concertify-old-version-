8f63be5a5f505ed4f73f6604ad6a4e8c3f1e3564
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-09-13 14:42:41 +0200

    tailwind is the shit

--- concertify/src/App.tsx
+++ concertify/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
+    <div className="App grid align-middle gap-5">
+      <div className="flex justify-center py-12">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1 className="text-4xl text-red-500">Hej Emilie og Tomine!</h1>
-      <h2>Hej Frederik</h2>
+      <h2 className="text-4xl">Hej Frederik</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
