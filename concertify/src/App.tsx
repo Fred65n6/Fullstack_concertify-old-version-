8cc97fa1f49dc3c31e9207c97032d6fadb49f7f0
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-09-14 15:06:59 +0200

    added game API

--- concertify/src/App.tsx
+++ concertify/src/App.tsx
@@ -1,35 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import ConcertGrid from "./components/ConcertGrid"
 import "./App.css";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App grid align-middle gap-5">
-      <div className="flex justify-center py-12">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-4xl text-red-500">Hej Emilie og Tomine!</h1>
-      <h2 className="text-4xl">Hej Frederik</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <div className="">
+    <ConcertGrid/>
     </div>
+
   );
 }
 
---
+++ concertify/src/hooks/useConcerts.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  playtime: number;
+}
+
+export interface GameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    apiClient
+      .get<GameResponse>("games")
+      .then((response) => {
+        setGames(response.data.results);
+        setIsLoading(false);
+        console.log(response);
+      })
+      .catch((error) => setError(error.message));
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
